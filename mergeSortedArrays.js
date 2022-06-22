42c371748541b6c19f67512eef5305ad166dd4b8
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-22 15:30:14 +0800

    tutor solution

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -1,22 +1,31 @@
 function mergeSortedArrays(arr1, arr2) {
-  arr2.forEach((item) => {
-    insertItem(arr1, item);
-  });
+  if (arr1.length === 0) {
+    return arr2;
+  }
+  if (arr2.length === 0) {
+    return arr1;
+  }
 
-  return arr1;
-}
+  const sortedArray = [];
+  let item1 = arr1[0];
+  let item2 = arr2[0];
+  let i = 0;
+  let j = 0;
 
-function insertItem(arr, item, cursor = 0) {
-  for (let i = cursor; i < arr.length; i++) {
-    if (arr[i] > item) {
-      console.log(`i: ${i}`);
-      arr.splice(i, 0, item);
-      return arr;
+  while (item1 || item2) {
+    console.log(item1, item2);
+    if (!item2 || item1 < item2) {
+      // 31 < undefined is false, item1 will never get pushed
+      // 31, unde
+      sortedArray.push(item1);
+      item1 = arr1[i++];
+    } else {
+      sortedArray.push(item2);
+      item2 = arr2[j++];
     }
   }
 
-  arr.push(item);
-  return arr;
+  return sortedArray;
 }
 
 const arr1 = [0, 3, 4, 31];
@@ -24,6 +33,3 @@ const arr2 = [4, 6, 30];
 
 result = mergeSortedArrays(arr1, arr2);
 console.log(result);
-// arr1.splice(3, 0, 5);
-// console.log(arr1);
-// console.log(insertItem(arr1, 32));
