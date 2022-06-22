56d07b55a7ca346d9dc4b5f14b49eb53e5aa7ee3
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-22 17:44:44 +0800

    finetune

--- hashtable.js
+++ hashtable.js
@@ -12,6 +12,7 @@ class HashTable {
   }
 
   set(key, value) {
+    // o(1)
     const index = this._hash(key);
     if (!this.data[index]) {
       this.data[index] = [];
@@ -23,13 +24,16 @@ class HashTable {
   }
 
   get(key) {
+    // most of time o(1), if collision, o(n) for bad case, e.g. very small hash table, collision all over the place
     const index = this._hash(key);
     // console.log(key, index);
     let result = null;
     this.data[index].forEach((item) => {
       //   console.log(item, item.key);
-      if (item.key === 'grapes') {
+      if (item.key === key) {
         result = item.value;
+        // return result;
+        // return item.value;
       }
     });
 
@@ -39,8 +43,11 @@ class HashTable {
 
 const myHashTable = new HashTable(50);
 myHashTable.set('grapes', 10000);
+myHashTable.set('apples', 100);
+myHashTable.set('bananas', 10);
 
 console.log(myHashTable.get('grapes'));
+console.log(myHashTable.data);
 
 // console.log(myHashTable.data);
 // console.log(myHashTable.data[23]);
