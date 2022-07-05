615afe735b4a4904cf505474a6ebe0305707b86a
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-05 16:32:17 +0800

    time complexity improved from a*b to a+b

--- compare_array.js
+++ compare_array.js
@@ -1,4 +1,4 @@
-function exists_common(arr1, arr2){
+function exists_common(arr1, arr2){   // o(a*b)
     for (let i = 0; i < arr1.length; i++) {
         if(arr2.includes(arr1[i])){
             return true
@@ -8,12 +8,26 @@ function exists_common(arr1, arr2){
     return false
 }
 
-arr1 = ['a','b','c','x']
+function containsCommonItem(arr1, arr2){  // o(a+b)
+    const hashtab = {}
+    for (let i = 0; i < arr1.length; i++) {
+        hashtab[arr1[i]] = true;        
+    }
+
+    for (let i = 0; i < arr2.length; i++) {
+        if(hashtab[arr2[i]]){
+            return true
+        }     
+    }
+    return false
+}
+
+arr1 = ['a','b','c','x', 'a']
 arr2 = ['z','y','i']
 arr3 = ['z','y','x']
 
-result1 = exists_common(arr1, arr2)
-result2 = exists_common(arr1, arr3)
+result1 = containsCommonItem(arr1, arr2)
+result2 = containsCommonItem(arr1, arr3)
 
 console.log(result1, result2);
 
