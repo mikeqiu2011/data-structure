f8f82b571e7f4e12b248f57672f7f7813dab6a07
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-09 11:14:06 +0800

    my solution for insert has bug

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -56,7 +56,7 @@ const tree = new BinarySearchTree();
 tree.insert(9);
 tree.insert(4);
 tree.insert(6);
-// tree.insert(20);
+tree.insert(20);
 // tree.insert(170);
 // tree.insert(15);
 // tree.insert(1);
