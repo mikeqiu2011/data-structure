2ccadeb9a5a428a9a2a380ce784e2cd6a725e671
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-09 22:04:47 +0800

    tutor solution using array

--- fibonacci.js
+++ fibonacci.js
@@ -1,26 +1,23 @@
+// 0,1,1,2,3,5,8,13,21,34,55,89,144..
+
+// given the index, return the corresponding value in the sequence
 function fibonacciRecursive(n) {
-  if (n == 0) {
-    return 0;
-  }
-  if (n == 1) {
-    return 1;
+  if (n < 2) {
+    return n;
   }
   return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 }
 
-const result = fibonacciRecursive(6);
+const result = fibonacciRecursive(8);
 console.log(result);
 
 function fibonacciIterative(n) {
-  let first = 0;
-  let second = 1;
-  for (let i = 1; i < n; i++) {
-    let temp = second;
-    second = first + second;
-    first = temp;
+  const array = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    array.push(array[i - 2] + array[i - 1]);
   }
-  return second;
+  return array;
 }
 
-const result2 = fibonacciIterative(8);
+const result2 = fibonacciIterative(10);
 console.log(result2);
