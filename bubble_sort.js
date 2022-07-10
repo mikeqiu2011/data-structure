7c2959e9d037529ca212804f982d38429356f7f7
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-10 08:40:07 +0800

    finetune

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,9 +1,12 @@
 const array = [6, 5, 3, 1, 8, 7, 2, 4];
 
-function bubble_sort(array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length - i; j++) {
+function bubbleSort(array) {
+  // O(n^2)
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length - 1; j++) {
       if (array[j] > array[j + 1]) {
+        // swap numbers
         let temp = array[j + 1];
         array[j + 1] = array[j];
         array[j] = temp;
@@ -14,5 +17,5 @@ function bubble_sort(array) {
   return array;
 }
 
-const result = bubble_sort(array);
+const result = bubbleSort(array);
 console.log(result);
