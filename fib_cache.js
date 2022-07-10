6ed478f19ff2bcf3a2ac1b6d1b1b34ad33b0ec72
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-10 21:15:59 +0800

    my implementation of dynamic programming of fib

--- fib_cache.js
+++ fib_cache.js
@@ -7,6 +7,27 @@ function fibonacci(n) {
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-const result = fibonacci(8);
+// const result = fibonacci(10);
+// console.log(result);
+// console.log(counter); // 61!!
+
+let counter2 = 0;
+const cache = {};
+function fibonacciMaster(n) {
+  counter2++;
+  if (n < 2) {
+    return n;
+  }
+  if (cache[n]) {
+    return cache[n];
+  } else {
+    const result = fibonacciMaster(n - 1) + fibonacciMaster(n - 2);
+    cache[n] = result;
+    return result;
+  }
+  //   return fibonacciMaster(n - 1) + fibonacciMaster(n - 2);
+}
+
+const result = fibonacciMaster(10);
 console.log(result);
-console.log(counter); // 61!!
+console.log(counter2); // 19!!
