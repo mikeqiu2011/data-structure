fb9c30e953cd4a816f203853298b52406e3b90ec
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-10 19:26:19 +0800

    cache implementation

---
+++ cache.js
@@ -0,0 +1,24 @@
+function addTo80(n) {
+  console.log('takes long time');
+  return n + 80;
+}
+
+let cache = {};
+
+function memorizedAddTo80(n) {
+  if (n in cache) {
+    return cache[n];
+  } else {
+    const result = addTo80(n);
+    cache[n] = result;
+    return result;
+  }
+}
+
+const test1 = memorizedAddTo80(5); // only the first time has real calculation
+const test2 = memorizedAddTo80(5);
+const test3 = memorizedAddTo80(5);
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
